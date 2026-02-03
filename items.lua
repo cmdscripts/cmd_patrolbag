309e68f70ab2c9539c7b02f26421c367d5ae4bec
cmd <123102218+cmdscripts@users.noreply.github.com>
2026-02-03 18:36:19 +0100

    Update items.lua

--- items.lua
+++ items.lua
@@ -1,11 +1,39 @@
--- ADD THIS TO YOUR ITEMS.LUA FROM OX INVENTORY
-
 ['patrolbag'] = {
-    label = 'Patrol Bag',
-    weight = 2500,
+    label = 'Patrolbag',
+    weight = 1000,
     stack = false,
     close = true,
-    consume = 0,
-    allowArmed = true,
-    client = { event = 'cmd_patrolbag:clientUse' }
+    client = {
+        event = 'cmd_patrolbag:clientUse'
+    }
+},
+
+['firstaid'] = {
+    label = 'First Aid Kit',
+    weight = 800,
+    stack = false,
+    close = true,
+    client = {
+        event = 'cmd_patrolbag:clientUse'
+    }
+},
+
+['manvbag'] = {
+    label = 'MANV Tasche',
+    weight = 1500,
+    stack = false,
+    close = true,
+    client = {
+        event = 'cmd_patrolbag:clientUse'
+    }
+},
+
+['kfz_kit'] = {
+    label = 'KFZ Verbandkasten',
+    weight = 500,
+    stack = false,
+    close = true,
+    client = {
+        event = 'cmd_patrolbag:clientUse'
+    }
 },
